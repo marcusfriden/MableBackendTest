511b21bc9cabd4a8c80f748a2806e0001b3bcd37
marcusfriden <m.fridn@gmail.com>
2026-09-03 14:14:30 +1000

    catch exceptions ins main and print error messages instead of full stack trace and extract print failures

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,33 +4,32 @@ import core.csv.CsvReader
 import transaction.TransactionFailure
 import transaction.TransactionLineParser
 import transaction.TransactionService
+import transaction.printIfNotEmpty
 import java.io.File
 
 fun main(args: Array<String>) {
-    require(args.size == 2) { "Usage: java -jar <application.jar> <balances.csv> <transactions.csv>" }
+    try {
+        require(args.size == 2) { "Usage: java -jar <application.jar> <balances.csv> <transactions.csv>" }
 
-    val accounts = CsvReader({ File(args[0]).reader() }, AccountLineParser())
-        .readAll()
-        .associateBy { it.accountNumber }
-    val accountRepository = InMemoryAccountRepository(accounts)
-    val transactionService = TransactionService(accountRepository)
+        val accounts = CsvReader({ File(args[0]).reader() }, AccountLineParser())
+            .readAll()
+            .associateBy { it.accountNumber }
+        val accountRepository = InMemoryAccountRepository(accounts)
+        val transactionService = TransactionService(accountRepository)
 
-    val transactions = CsvReader({ File(args[1]).reader() }, TransactionLineParser()).readAll()
-    val result = transactionService.process(transactions)
+        val transactions = CsvReader({ File(args[1]).reader() }, TransactionLineParser()).readAll()
+        val result = transactionService.process(transactions)
 
-    println("Updated Account Balances:")
-    result.accounts.forEach { (accountNumber, account) ->
-        println("  ${accountNumber.value}: ${account.balance}")
-    }
-
-    if (result.failures.isNotEmpty()) {
-        println()
-        println("Failed Transactions:")
-        result.failures.forEach { failure ->
-            when (failure) {
-                is TransactionFailure.InsufficientFunds ->
-                    println("  InsufficientFunds: ${failure.transaction.from.value} -> ${failure.transaction.to.value}, amount: ${failure.transaction.amount}")
-            }
+        println("Updated Account Balances:")
+        result.accounts.forEach { (accountNumber, account) ->
+            println("  ${accountNumber.value}: ${account.balance}")
+        }
+        result.failures.printIfNotEmpty()
+    } catch (e: Exception) {
+        if (e is IllegalArgumentException) {
+            println(e.message)
+        } else {
+            System.err.println("Error: ${e.message}")
         }
     }
 }
--- src/main/kotlin/transaction/TransactionFailure.kt
+++ src/main/kotlin/transaction/TransactionFailure.kt
@@ -3,3 +3,16 @@ package transaction
 sealed class TransactionFailure(val transaction: Transaction) {
     class InsufficientFunds(transaction: Transaction) : TransactionFailure(transaction)
 }
+
+fun List<TransactionFailure>.printIfNotEmpty() {
+    if (isNotEmpty()) {
+        println()
+        println("Failed Transactions:")
+        forEach { failure ->
+            when (failure) {
+                is TransactionFailure.InsufficientFunds ->
+                    println("  InsufficientFunds: ${failure.transaction.from.value} -> ${failure.transaction.to.value}, amount: ${failure.transaction.amount}")
+            }
+        }
+    }
+}
